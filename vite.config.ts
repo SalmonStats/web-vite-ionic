5288aab1e9d527af47013d52a7ac0ae9c3bee95e
tkgstrator <nasawake.am@gmail.com>
2022-08-14 04:12:19 +0900

    - 環境変数を正しく読み込むように修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import Vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
 import ViteYaml from "@modyfi/vite-plugin-yaml";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
-  },
-  plugins: [Vue(), VitePWA(), ViteYaml()],
-  define: {
-    import.meta.env.VITE_APP_URL: "https://api.splatnet2.com/v1",
-  },
+export default defineConfig(({ command, mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), "") };
+  return {
+    resolve: {
+      alias: [{ find: "@", replacement: "/src" }],
+    },
+    plugins: [Vue(), VitePWA(), ViteYaml()],
+    define: {
+      VITE_APP_URL: JSON.stringify(process.env.VITE_APP_URL),
+    },
+  }
 });
